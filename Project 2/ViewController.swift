ecb15d3cda68424a68c8d5df9051da5480deb87b
Huy Tran <huytran172@gmail.com>
2023-04-06 01:38:19 -0400

    feat: Pan in current user location

--- Project 2/ViewController.swift
+++ Project 2/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    private let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+    private func setupMap() {
+        mapView.showsUserLocation = true
+        
+        guard let currentLocation = locationManager.location else { return }
+        print(currentLocation)
+        let radiusInMeters: CLLocationDistance = 1000000
 
+        // Set the region around user location
+        let region = MKCoordinateRegion(
+            center: currentLocation.coordinate,
+            latitudinalMeters: radiusInMeters,
+            longitudinalMeters: radiusInMeters
+        )
+        mapView.setRegion(region, animated: true)
+    }
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        setupMap()
+    }
+}
